32fc1613cecbfcb318a652a4f5d1453b9b71f64c
Iliya <72569914+yailia@users.noreply.github.com>
2026-03-16 20:07:25 +0300

    Configure Astro for GitHub Pages deployment

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,5 +5,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yailia.github.io',
+  base: '/met4-frontend',
+  output: 'static',
   integrations: [svelte()]
 });
